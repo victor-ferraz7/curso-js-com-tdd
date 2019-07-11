9a234c7df85beb9c0b176e53388fb7d30cb87a40
Your Name <you@example.com>
2019-07-10 23:19:26 -0300

    10/07

---
+++ es6/04-templateLiterals.js
@@ -0,0 +1,14 @@
+const sampa = {
+    moro: "Peruíbe",
+    desejo: "São Paulo"
+}
+
+console.log(`Eu moro em ${sampa.moro} e desejo me mudar para ${sampa.desejo}`)
+
+const coisas = 
+`Bola de basquete
+Bicicleta
+tenis
+`
+
+console.log(coisas)
