82ecd67d3271c44e577cdf2c11409e84d35ada78
Your Name <you@example.com>
2019-07-14 23:04:00 -0300

    Default Parameters

---
+++ es6/05-defaultParameters.js
@@ -0,0 +1,7 @@
+
+function corre(nome = "Mlkãoooo"){ 
+    console.log(` COOOOOOOOOOOOOOOOOOOORRREEEEEE ${nome}`)
+}
+
+corre() // A função foi chamada e não houve passagem de parâmetro, logo o valor que será assumido será o default (Mlkãaaao)
+corre("Filipe")
