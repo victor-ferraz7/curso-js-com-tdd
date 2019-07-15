02f78e0aeb81144c63dcaced1f726f3e0f8aef1f
Your Name <you@example.com>
2019-07-14 23:22:13 -0300

    Exemplo de startsWith(), endsWith() e includes()

---
+++ es6/05-newStringsMethods.js
@@ -0,0 +1,5 @@
+let frase = "O Rato roeu a roupa do rei de Roma"
+
+console.log(frase.startsWith('O')) // retorna um boolean
+console.log(frase.endsWith("r"))
+console.log(frase.includes("roupa")) // verifica se há na frase o parâmetro informado ("roupa")
