3d645b41434765a71cadae99274104f0cc1a68c4
Your Name <you@example.com>
2019-07-16 11:05:28 -0300

    Exemplo de passagem de parametro para função utilizando array com o spread operator

---
+++ es6/08-spreadOperator3.js
@@ -0,0 +1,7 @@
+function fazerPastel(ingrediente1,ingrediente2,ingrediente3){
+    console.log(`O pastel escolido foi de ${ingrediente1} com ${ingrediente2} e ${ingrediente3}`)
+}
+
+let ingredientes = ["Queijo Prato","Brocolis","Alho Frito"]
+
+fazerPastel(...ingredientes)
