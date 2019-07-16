6b94818eab4e3676e485d08ae2d8403a42345f01
Your Name <you@example.com>
2019-07-16 10:46:44 -0300

    Exemplo unindo arrays com spread e inserindo string

---
+++ es6/08-spreadOperator2.js
@@ -0,0 +1,8 @@
+const comidaDoNorte = ["Maniçoba","Tacacá","Caruru Paraense"]
+const comidaDoNordeste = ["Acarajé","Buchada de Bode","Carne de Sol"]
+const comidaDoSudeste = ["Pão de Queijo","Feijão tropeiro","Frango com Quiabo"]
+const comidasDoSul = ["Costela Assada","Espeto Corrido","Matambre Recheado"]
+
+const comidasDoBrasil = [...comidaDoNorte, ...comidaDoNordeste, ...comidaDoSudeste, ...comidasDoSul,"E Muito Mais ..."]
+
+console.log(comidasDoBrasil)
