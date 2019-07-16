3b2eb500753bd4dda874fe68795faf73fe6e055a
Your Name <you@example.com>
2019-07-16 10:08:43 -0300

    Exemplo de Desctructuring com Array

---
+++ es6/07-destructuringArray.js
@@ -0,0 +1,7 @@
+const escalaDeDoh = ["do","re","mi","fa","sol","la","si"]
+
+const [tonica, segunda, terca, quarta, quinta, sexta, setima] = escalaDeDoh
+
+console.log(`
+As notas ${tonica}, ${terca} e ${quinta} formam o acorde de Dó
+`)
